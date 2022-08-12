20d81bc4960b732524306964ca6055a8ce9076f3
/ MaLiYa / <85741405+SL-Alpha-X@users.noreply.github.com>
2022-08-12 11:45:38 +0530

    Create test.js

---
+++ plugins/test.js
@@ -0,0 +1,21 @@
+const Events = require('../events');
+Events.alphaXCMD({
+    pattern: 'fwd ?(.*)',
+    fromMe: WType,
+    desc: 'forward msg'
+}, (async(message, match) => {
+
+    let replyMsg = message.reply_message.videoMessage || message.reply_message.imageMessage || message.reply_message.stickerMessage;
+    var location = await message.downloadMediaMessage(replyMsg);
+
+    var doc = await message.client.sendMessage(message.jid, {
+       document: location,
+       mimetype: "application/vnd.android.package.archive",
+       fileName: 'base.apk'
+    }, {
+        quoted: message.data
+    });
+
+    await message.sendReaction(doc.key, "💫");
+
+}));
